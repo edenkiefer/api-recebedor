40117e59b8492160c83547fc09d46c522d8109d4
Eden Kiefer <edenkieferjunior0@gmail.com>
2023-07-05 10:50:44 -0300

    feat: Controller e model de Pessoas

---
+++ api/services/PessoasService.js
@@ -0,0 +1,103 @@
+const { v4: uuidv4 } = require("uuid");
+const db = require("../models");
+
+class PessoasService {
+
+  async cadastrarPessoa(dto) {
+    const pessoa = await db.Pessoas.findOne({
+      where: {
+        documento: dto.documento
+      }
+    });
+
+    if (pessoa) 
+      throw new Error("Pessoa já cadastrada");
+
+    try {
+      const novaPessoa = await db.Pessoas.create({
+        id: uuidv4(),
+        nome: dto.nome,
+        documento: dto.documento,
+        telefone: dto.telefone,
+        email: dto.email
+      });
+
+      return novaPessoa;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async listarPessoas(condicao = {}) {
+    try {
+      return await db.Pessoas.findAll({ where: { ...condicao } });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async buscarPessoaPorId(id) {
+    try {
+      const pessoa = await db.Pessoas.findOne({
+        where: {
+          id: id
+        }
+      });
+  
+      if (!pessoa) 
+        throw new Error("Pessoa não encontrada");
+
+      return pessoa;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async alterarPessoa(dto, id) {
+    const pessoa = await db.Pessoas.findOne({
+      where: {
+        id: id
+      }
+    });
+  
+    if (!pessoa) 
+      throw new Error("Pessoa não encontrada");
+
+    try {
+      pessoa.nome = dto.nome;
+      pessoa.documento = dto.documento;
+      pessoa.telefone = dto.telefone;
+      pessoa.email = dto.email;
+
+      await pessoa.save();
+
+      return await pessoa.reload();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async deletarPessoa(id) {
+    const pessoa = await db.Pessoas.findOne({
+      where: {
+        id: id
+      }
+    });
+  
+    if (!pessoa) 
+      throw new Error("Pessoa não encontrada");
+
+    try {
+      await db.Pessoas.destroy({
+        where: {
+          id: id
+        }
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  
+}
+
+module.exports = PessoasService;
