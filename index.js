6e5502008c1ddddd20c921e168ec21cfcd4d8e45
Eden Kiefer <edenkieferjunior0@gmail.com>
2023-07-05 07:07:17 -0300

    chore: Adicionando o Dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send({ message: 'Teste' }));
 
