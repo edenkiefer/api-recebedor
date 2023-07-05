7a7a0c36fe5cd4227b992cd25157183d03b8264a
Eden Kiefer <edenkieferjunior0@gmail.com>
2023-07-05 08:17:40 -0300

    feat:Criando a conexão com o banco

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./api/models");
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send({ message: 'Teste' }));
-
 app.use(bodyParser.json());
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+app.get("/", (req, res) => res.send({ message: "Teste" }));
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("A conexão com o banco foi estabelecida com sucesso!");
+    app.listen(port, async () => console.log(`Servidor rodando na porta ${port}`));
+  })
+  .catch(err => console.log("Não foi possível conectar ao banco de dados:", err));
 
 module.exports = app;
