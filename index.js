bc8447f97f72790bcc46d6abeeb5d6f256a0b192
Eden Kiefer <edenkieferjunior0@gmail.com>
2023-07-05 07:31:29 -0300

    feat: Ultilizando o body-parser no App

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send({ message: 'Teste' }));
 
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
 module.exports = app;
