de26ca52a57cc6d1abe7ede01467585651921c7e
Eden Kiefer <edenkieferjunior0@gmail.com>
2023-07-05 16:18:01 -0300

    feat: Adicionando rotas de transações

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const bodyParser = require("body-parser");
 const pessoas = require("./PessoasRoute.js");
+const transacoes = require("./TransacaoRoute.js");
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
-    pessoas
+    pessoas,
+    transacoes
   );
 };
