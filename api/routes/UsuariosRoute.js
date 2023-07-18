2228bd1505f2acb32c3f5f9aa43cc80de9eeb7e9
Eden Kiefer <edenkieferdev@gmail.com>
2023-07-18 09:35:07 -0300

    feat: Criando middleware de usuário autenticado

--- api/routes/UsuariosRoute.js
+++ api/routes/UsuariosRoute.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
 const UsuariosController = require("../controllers/UsuariosController");
+const autenticado = require("../middlewares/autenticado");
 
 const router = Router();
 
 router
-  .post("/usuarios", UsuariosController.cadastrarUsuario)
+  .post("/usuarios", UsuariosController.cadastrarUsuario);
+
+router.use(autenticado);
+
+router
   .get("/usuarios", UsuariosController.listarUsuarios)
   .get("/usuarios/:id", UsuariosController.buscarUsuarioPorId)
   .put("/usuarios/:id", UsuariosController.alterarUsuario)
