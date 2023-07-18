1f2baca7377073e75b41de084f3e8605d0b1a048
Eden Kiefer <edenkieferdev@gmail.com>
2023-07-18 09:34:14 -0300

    feat: Adicionando rotas de autenticação

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,10 +3,12 @@ const pessoas = require("./PessoasRoute.js");
 const transacoes = require("./TransacaoRoute.js");
 const usuarios = require("./UsuariosRoute.js");
 const roles = require("./RolesRoute.js");
+const auth = require("./AuthRoute.js");
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
+    auth,
     usuarios,
     roles,
     pessoas,
