8f6d856f4a6e9de76b28a69007f27ef0096e51e4
Eden Kiefer <edenkieferdev@gmail.com>
2023-07-18 12:22:40 -0300

    feat: Adicionando validação de roles a transações

--- api/routes/TransacaoRoute.js
+++ api/routes/TransacaoRoute.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const TransacoesController = require("../controllers/TransacoesController");
+const roles = require("../middlewares/roles");
 
 const router = Router();
 
 router
-  .post("/transacoes", TransacoesController.cadastrarTransacao)
-  .get("/transacoes", TransacoesController.listarTransacoes)
-  .get("/transacoes/:id", TransacoesController.buscarTransacaoPorId)
-  .put("/transacoes/:id", TransacoesController.alterarTransacao)
-  .delete("/transacoes/:id", TransacoesController.deletarTransacao);
+  .post("/transacoes", roles(["admin", "usuario"]), TransacoesController.cadastrarTransacao)
+  .get("/transacoes", roles(["admin"]), TransacoesController.listarTransacoes)
+  .get("/transacoes/:id", roles(["admin", "usuario"]), TransacoesController.buscarTransacaoPorId)
+  .put("/transacoes/:id", roles(["admin", "usuario"]), TransacoesController.alterarTransacao)
+  .delete("/transacoes/:id", roles(["admin"]), TransacoesController.deletarTransacao);
 
 module.exports = router;
